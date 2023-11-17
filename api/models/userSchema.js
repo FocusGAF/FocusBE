bd105b1b2bda26d25119a5aa588717aec5e62e81
Skahrr <1dgonascaaron@gmail.com>
2023-11-17 16:40:13 +0000

    login done

--- api/models/userSchema.js
+++ api/models/userSchema.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   // phone: { type: Number, required: true },
   // address: { type: String, required: true },
   password: { type: String, required: true },
-  // projects: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Project" }],
+  projects: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Project" }],
 });
 
 module.exports = mongoose.model("User", userSchema);
