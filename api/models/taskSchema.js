a66125ac80ce55e0ad38c83169fd365c8db23be8
Fersoher <74140565+Fersoher@users.noreply.github.com>
2023-11-18 12:53:11 +0000

    Enum y priority

--- api/models/taskSchema.js
+++ api/models/taskSchema.js
@@ -4,11 +4,16 @@ const taskSchema = new mongoose.Schema({
   name: { type: String },
   budget: { type: Number },
   deadline: { type: Date },
-  status: { type: String},
+  status: {  type: String,
+    enum : ['To do','In progress','Done'],
+    default: 'To do' },
   participants: [{ type: mongoose.SchemaTypes.ObjectId, ref: "User" }],
   notes: { type: String },
   description: { type: String },
   files: { type: URL },
+  priority:{  type: String,
+    enum : ['Low','Medium','High'],
+    },
   subtasks: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Subtask" }] 
 });
 
