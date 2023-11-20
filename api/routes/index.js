ed9476964077eb74595ac09e53deb84d0f1e3cc1
Guayasen <guayasen.ojeda.deniz@gmail.com>
2023-11-20 11:54:06 +0000

    boardController

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 router.use("", require("./auth.routes.js"));
 router.use("/users", require("./user.routes.js"));
+router.use("/boards", require("./board.routes.js"));
 
 module.exports = router;
