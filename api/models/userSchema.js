28333d47c188169c63922992c3f63171820b35f2
Skahrr <1dgonascaaron@gmail.com>
2023-11-20 15:06:53 +0000

    random comments

--- api/models/userSchema.js
+++ api/models/userSchema.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  // name: { type: String, required: true },
-  // last_name: { type: String, required: true },
+  name: { type: String, required: true },
+  last_name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  // phone: { type: Number, required: true },
-  // address: { type: String, required: true },
+  phone: { type: Number, required: true },
+  address: { type: String, required: true },
   password: { type: String, required: true },
   projects: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Project" }],
 });
