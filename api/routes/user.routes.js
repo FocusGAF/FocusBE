453bc1b601efd6d77a306a223344ba3e8d54b331
Fernando Sosa Hernández <74140565+Fersoher@users.noreply.github.com>
2023-11-20 10:32:12 +0000

    Crud-basico-project

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,7 +3,7 @@ const { checkAuth } = require("../middlewares/index");
 const {getMyProfile, getAllUsers, getOneUser, deleteUser, updateUser, createUser} = require("../controllers/user.controller")
 router.get("/myProfile", checkAuth, getMyProfile)
 router.get("/", checkAuth, getAllUsers)
-router.post("", checkAuth, createUser)
+router.post("/", checkAuth, createUser)
 router.get("/:id", checkAuth, getOneUser)
 router.put("/:id", checkAuth, updateUser)
 router.delete("/:id", checkAuth, deleteUser)
