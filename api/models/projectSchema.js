21d203e88d143e4c44627e24a5f1f6afba0d4dbe
guayasen <guayasen.ojeda.deniz@gmail.com>
2023-11-24 10:38:30 +0000

    faker installation

--- api/models/projectSchema.js
+++ api/models/projectSchema.js
@@ -8,6 +8,7 @@ const projectSchema = new mongoose.Schema({
       owner: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
       admins: [{ type: mongoose.SchemaTypes.ObjectId, ref: "User" }],
       members: [{ type: mongoose.SchemaTypes.ObjectId, ref: "User" }],
+      
     },
   },
   springs: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Board" }],
