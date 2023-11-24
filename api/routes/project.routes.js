39f53842d836fce59bcf1751781945a48831a2ba
Skahrr <1dgonascaaron@gmail.com>
2023-11-24 13:07:11 +0000

    new project route

--- api/routes/project.routes.js
+++ api/routes/project.routes.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
 const { checkAuth } = require("../middlewares/index");
-const { getAllProjects, getOneProject, deleteProject, updateProject, createProject} = require("../controllers/project.controller")
-router.get("/", checkAuth, getAllProjects)
-router.post("/", checkAuth, createProject)
-router.get("/:id", checkAuth, getOneProject)
-router.put("/:id", checkAuth, updateProject)
-router.delete("/:id", checkAuth, deleteProject)
-
-module.exports= router
+const {
+  getAllProjects,
+  getOneProject,
+  deleteProject,
+  updateProject,
+  createProject,
+  getProjectByInvCode,
+} = require("../controllers/project.controller");
+router.get("/", checkAuth, getAllProjects);
+router.post("/", checkAuth, createProject);
+router.get("/:id", checkAuth, getOneProject);
+router.get("/invite/:id", checkAuth, getProjectByInvCode);
+router.put("/:id", checkAuth, updateProject);
+router.delete("/:id", checkAuth, deleteProject);
+module.exports = router;
